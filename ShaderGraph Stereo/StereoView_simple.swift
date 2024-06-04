ca2390aec84bb66ba8a77d4431625d92bdf80154
Hal Mueller <halmueller@gmail.com>
2024-06-04 14:35:28 -0700

    Add unoptimized version.

--- ShaderGraph Stereo/StereoView_simple.swift
+++ ShaderGraph Stereo/StereoView_simple.swift
@@ -12,7 +12,7 @@ import SwiftUI
 struct StereoView: View {
     var imagePair: StereoPair
     
-    @State private var stereo: ShaderGraphMaterial?
+    @State private var stereoMaterial: ShaderGraphMaterial?
     @State private var leftTexture: TextureResource?
     @State private var rightTexture: TextureResource?
     
@@ -30,7 +30,7 @@ struct StereoView: View {
                 RealityView { content in // this closure is asynchronus
                     print("\(#function) make \(imagePair.id)")
                     do {
-                        stereo = try await ShaderGraphMaterial(named: "/Root/StereoImage",
+                        stereoMaterial = try await ShaderGraphMaterial(named: "/Root/StereoImage",
                                                                from: "StereoMaker.usda",
                                                                in: realityKitContentBundle)
                         updateStereo(content: content)
@@ -48,18 +48,15 @@ struct StereoView: View {
 
 extension StereoView {
     func updateStereo(content: RealityViewContent) {
-        // !!!: Saved state in the RV Content is an ugly hack that saves a bunch of memory leakage.
-        if let imagePlane = content.entities.first as? ModelEntity,
-           imagePlane.name == imagePair.id { return }
         do {
             // Each pass through this closure leaks memory. Don't do it if we don't have to. This function is called for every
             // Window resize event. Don't reload the textures if it's the same one as last time.
-            if var stereo {
+            if var stereoMaterial {
                 let leftTexture = try TextureResource.load(named: imagePair.leftImageName)
-                try stereo.setParameter(name: "LeftImage", value: .textureResource(leftTexture))
+                try stereoMaterial.setParameter(name: "LeftImage", value: .textureResource(leftTexture))
                 
                 let rightTexture = try TextureResource.load(named: imagePair.rightImageName)
-                try stereo.setParameter(name: "RightImage", value: .textureResource(rightTexture))
+                try stereoMaterial.setParameter(name: "RightImage", value: .textureResource(rightTexture))
                 
                 // TODO: This locks us in to one aspect ratio, that of the first image viewed. Hardcode this, or perhaps tweak the Plane's transform to match varying aspect ratios.
                 let planeWidth: Float
@@ -74,15 +71,16 @@ extension StereoView {
                 }
                 print("aspectRatio \(aspectRatio) texture w/h: \(rightTexture.width) \(rightTexture.height) plane w/h \(planeWidth) \(planeHeight)")
                 
+                // In an even earlier version, I was removing the imagePlane and recreating it. On each pass. Ouch.
                 if let imagePlane = content.entities.first as? ModelEntity {
                     // !!!: Replacing the materials array leaks memory. It leaks more on device.
-                    imagePlane.model?.materials = [stereo]
+                    imagePlane.model?.materials = [stereoMaterial]
                     imagePlane.name = imagePair.id
                 } else {
                     // FIXME: we keep this Plane Entity around for duration of the View, without resizing, even if images vary in size.
                     let newPlane = ModelEntity(mesh: .generatePlane(width: planeWidth, height: planeHeight))
                     newPlane.name = imagePair.id
-                    newPlane.model?.materials = [stereo]
+                    newPlane.model?.materials = [stereoMaterial]
                     // TODO: try adjusting the transform to match image aspect.
                     // Z value of 0 produces terrible Z fighting
                     newPlane.transform.translation = [0, 0, 0.01]
